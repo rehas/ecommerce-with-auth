6d4dc02ac548f9c58b8fdc89432b28dfcbafb835
RehaS <beratson@gmail.com>
2018-08-03 14:00:24 +0200

    db file changed to point server dburl

--- db.js
+++ db.js
@@ -1,5 +1,7 @@
 const Sequelize = require('sequelize')
 
+const dbUrl = process.env.DATABASE_URL || "postgres://postgres:secret@localhost:5432/postgres"
+
 const sequelize = new Sequelize('postgres://postgres:secret@localhost:5432/postgres')
 
 module.exports = sequelize
